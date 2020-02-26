83739fea996a305dcf61fcaff46477e5bf62ffd4
Miracle_lms <37183438+Jokertion@users.noreply.github.com>
2020-02-26 23:30:19 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,137 @@
+import os
+import re
+import yaml
+from lxml.html import fromstring, HtmlElement
+from lxml.html import etree
+from urllib.parse import urlparse, urljoin
+from .defaults import USELESS_TAG, TAGS_CAN_BE_REMOVE_IF_EMPTY, USELESS_ATTR, HIGH_WEIGHT_ARRT_KEYWORD
+
+
+def normalize_node(element: HtmlElement):
+    # 规范化节点（清除无用节点）
+    etree.strip_elements(element, *USELESS_TAG)  # 清除无用节点，['style', 'script', 'link', 'video', 'iframe', 'source', 'picture', 'header', 'blockquote']
+    for node in iter_node(element):
+        # inspired by readability.
+        if node.tag.lower() in TAGS_CAN_BE_REMOVE_IF_EMPTY and is_empty_element(node):
+            remove_node(node)
+
+        # merge text in span or strong to parent p tag
+        # 清除p中的span和strong标签
+        if node.tag.lower() == 'p':
+            etree.strip_tags(node, 'span')
+            etree.strip_tags(node, 'strong')
+
+        # if a div tag does not contain any sub node, it could be converted to p node.
+        # 将没有子孙标签的div、span标签 更改为p(文本)标签
+        if node.tag.lower() == 'div' and not node.getchildren():
+            node.tag = 'p'
+
+        if node.tag.lower() == 'span' and not node.getchildren():
+            node.tag = 'p'
+
+        # remove empty p tag
+        # 移除空的p标签
+        if node.tag.lower() == 'p' and not node.xpath('.//img'):
+            if not (node.text and node.text.strip()):
+                drop_tag(node)
+
+        # 通过标签class属性判断，来去掉没用的标签
+        class_name = node.get('class')
+        if class_name:
+            for attribute in USELESS_ATTR:
+                if attribute in class_name:
+                    remove_node(node)
+                    break
+
+
+def pre_parse(html):
+    # 剔除换行标签
+    html = re.sub('</?br.*?>', '', html)
+    element = fromstring(html)  # 将html解析成 HtmlElement对象
+
+    # 规范化节点（清除无用节点）
+    normalize_node(element)
+    return element
+
+
+def remove_noise_node(element, noise_xpath_list):
+    noise_xpath_list = noise_xpath_list or config.get('noise_node_list')
+    if not noise_xpath_list:
+        return
+    for noise_xpath in noise_xpath_list:
+        nodes = element.xpath(noise_xpath)
+        for node in nodes:
+            remove_node(node)
+    return element
+
+
+def iter_node(element: HtmlElement):
+    yield element
+    for sub_element in element:
+        if isinstance(sub_element, HtmlElement):
+            yield from iter_node(sub_element)
+
+
+def remove_node(node: HtmlElement):
+    """
+    this is a in-place operation, not necessary to return
+    :param node:
+    :return:
+    """
+    parent = node.getparent()
+    if parent is not None:
+        parent.remove(node)
+
+
+def drop_tag(node: HtmlElement):
+    """
+    only delete the tag, but merge its text to parent.
+    :param node:
+    :return:
+    """
+    parent = node.getparent()
+    if parent is not None:
+        node.drop_tag()
+
+
+def is_empty_element(node: HtmlElement):
+    return not node.getchildren() and not node.text
+
+
+def pad_host_for_images(host, url):
+    """
+    网站上的图片可能有如下几种格式：
+
+    完整的绝对路径：https://xxx.com/1.jpg
+    完全不含 host 的相对路径： /1.jpg
+    含 host 但是不含 scheme:  xxx.com/1.jpg 或者  ://xxx.com/1.jpg
+
+    :param host:
+    :param url:
+    :return:
+    """
+    if url.startswith('http'):  # http开头的其实也把https包含在内了
+        return url
+    parsed_uri = urlparse(host)
+    scheme = parsed_uri.scheme
+    if url.startswith(':'):
+        return f'{scheme}{url}'
+    if url.startswith('//'):
+        return f'{scheme}:{url}'
+    return urljoin(host, url)
+
+
+def read_config():
+    if os.path.exists('.gne'):
+        with open('.gne', encoding='utf-8') as f:
+            config_text = f.read()
+        config = yaml.safe_load(config_text)
+        return config
+    return {}
+
+
+def get_high_weight_keyword_pattern():
+    return re.compile('|'.join(HIGH_WEIGHT_ARRT_KEYWORD), flags=re.I)
+
+
+config = read_config()
